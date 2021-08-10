35a007bcda583ddb8b2fb2dd3f4c2b566a43bd1b
unknown <hoangnd997@gmail.com>
2021-08-10 16:49:23 +0700

    add user_list_view, get_users

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,9 @@ from .views import (
     logout_view,
     register_view,
     success_view,
-    register_api
+    register_api,
+    user_list_view,
+    get_users
 )
 
 # just for debug
@@ -18,13 +20,19 @@ from django.conf.urls.static import static
 #--------------------------#
 
 urlpatterns = [
+    # Views
     path('login/',login_view, name='user-login'),
     path('logout/',logout_view, name='user-logout'),
     path('register/',register_view, name='user-register'),
-    path('register-api/',register_api, name='user-register-api'),
     path('success/',success_view, name='user-success'),
+    path('user/list/',user_list_view, name='user-list'),
     path('user/<int:pk>/profile/',UserProfileView.as_view(), name='user-profile'),
     path('user/<int:pk>/update/',UserProfileUpdate.as_view(), name='user-update'),
+
+    # APIs
+    path('register-api/',register_api, name='user-register-api'),
+    path('get-users/',get_users, name='get-users'),
+
 ]
 
 if settings.DEBUG :
