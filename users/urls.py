5f62675de13803de12fed4dbc4fdf1fa1ac43cf4
unknown <hoangnd997@gmail.com>
2021-08-11 22:05:55 +0700

    fix profile_view and some minor database bug

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,5 @@
 from django.urls import path
 from . import views
-from .views import (
-    UserLoginView, 
-    UserLogoutView, 
-    UserRegisterView, 
-    UserProfileView,
-    UserProfileUpdate,
-    login_view,
-    logout_view,
-    register_view,
-    success_view,
-    register_api,
-    user_list_view,
-    get_users
-)
 
 # just for debug
 from django.conf import settings
@@ -22,26 +8,27 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # Views
-    path('login/',login_view, name='user-login'),
-    path('logout/',logout_view, name='user-logout'),
-    path('register/',register_view, name='user-register'),
-    path('success/',success_view, name='user-success'),
-    path('user/list/',user_list_view, name='user-list'),
-    path('user/<int:pk>/profile/',UserProfileView.as_view(), name='user-profile'),
-    path('user/<int:pk>/update/',UserProfileUpdate.as_view(), name='user-update'),
-
-
+    path('login/',views.login_view, name='user-login'),
+    path('logout/',views.logout_view, name='user-logout'),
+    path('register/',views.register_view, name='user-register'),
+    path('success/',views.success_view, name='user-success'),
+    path('user/list/',views.user_list_view, name='user-list'),
+    path('profile/', views.profile_view, name='user-profile'),
+    # path('user/<int:pk>/profile/',views.UserProfileView.as_view(), name='user-profile'),
+    # path('user/<int:pk>/update/',views.UserProfileUpdate.as_view(), name='user-update'),
+    
 
     # ====== User functions =====
-    path('get-users', views.get_users, name='get-users'),
-    path('activate-user', views.activate_user, name='activate-user'),
-    path('delete-user', views.delete_user, name='delete-user'),
-    path('edit-user-info', views.edit_user_info, name='edit-user-info'),
-    path('change-password', views.change_password, name='change-password'),
-    path('profile', views.profile, name='profile'),
+    # path('get-users/', views.get_users, name='get-users'),
+    # path('activate-user/', views.activate_user, name='activate-user'),
+    # path('delete-user/', views.delete_user, name='delete-user'),
+    # path('edit-user-info/', views.edit_user_info, name='edit-user-info'),
+    # path('change-password/', views.change_password, name='change-password'),
+    
     # APIs
-    path('register-api/',register_api, name='user-register-api'),
-    path('get-users/',get_users, name='get-users'),
+    path('register-api/', views.register_api, name='user-register-api'),
+    path('get-users/', views.get_users, name='get-users'),
+    
 
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.urls.base import reverse
 from users.models import Department, Position, UserInfor
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.http import JsonResponse
 from django.views.generic import CreateView, DetailView, UpdateView
@@ -11,7 +11,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
-import users.models as db
 
 # LIST_GROUPS=[
 #     "Trợ Lý",
@@ -163,186 +162,46 @@ def register_view(request):
     }
     return render(request, 'users/register.html', context)
 
-# API here
-
-def JsonResponseError(msg):
-    ''' Trả về ({"status":-1,"msg":msg}) '''
-    return JsonResponse({"status":-1,"msg":msg})
-def JsonResponseSuccess(data):
-    ''' Trả về ({"status":0,"data":data}) '''
-    return JsonResponse({"status":0,"data":data})
-
 @login_required
-def register_api(request):
-    '''
-    - Create new user (has same department as creater)
-    '''
-    msg = ''
-    if request.method == 'POST':
-        username = request.POST.get('username', '')
-        password=request.POST.get('password', '')
-        fullname=request.POST.get('fullname', '')
-        # Validate POST data
-        if username and password and fullname:
-            if User.objects.filter(username=username).count() > 0:
-                msg="Tên tài khoản đã tồn tại"
-        else:
-            msg="Hãy điền đầy đủ thông tin"
-        # Create User if data is validated
-        if msg == '':
-            user = User.objects.create_user(username=username, password=password, first_name=fullname, is_active=False)
-            user.info.department = request.user.info.department
-            user.info.position = Position.objects.get(alias='tk')
-            user.info.save()
-            return JsonResponseSuccess('Tạo tài khoản thành công!')
-    return JsonResponseError(msg)
-
-
-
-# =================== CRUD USER =============
-
-@login_required
-def get_users(request):
-    ''' Auto get users by his department'''
-    onlyEditor=str(request.GET.get('onlyEditor',''))
-    # Retrive user department
-    data={
-        'users':[],
-        'owner_group':[group.name for group in request.user.po.all()]
-    }
-    queryset=db.UserInfor.objects.filter(department=request.user.Userinfo.department).select_related('user')
-    
-    for user_info in queryset:
-        flag=False
-        if  onlyEditor != '1':
-            flag=True
-        elif user_info.user.groups.filter(name=LIST_GROUPS[0]).count()>0:
-            flag=True
-        if flag==True:
-            data['users'].append({
-                "id":user_info.user.id,
-                "first_name":user_info.user.first_name,
-                "last_login":user_info.user.last_login,
-                "date_joined":user_info.user.date_joined,
-                "username":user_info.user.username,
-                "is_active":user_info.user.is_active,
-                "groups":[group.name for group in user_info.user.groups.all()]
-                 })
-        # data['allUsers'].append({"id":user_info.user.id,"first_name":user_info.user.first_name,"username":user_info.user.username,"is_active":user_info.user.is_active})
-   
-    return JsonResponseSuccess(data)
-
-@login_required
-def activate_user(request):
-    if request.user.groups.filter(name=LIST_GROUPS[1]).count() == 0:
-        return JsonResponseError('Không đủ thẩm quyền')
-    userId=request.POST.get('userId','')
-    if not userId:
-        return JsonResponseError('Không đủ dữ liệu')
-    userForChanged=User.objects.get(id=userId)
-    if (userForChanged==request.user):
-        return JsonResponseError('Không thể huỷ kích hoạt chính mình')
-    if (userForChanged.user_info.department!=request.user.user_info.department):
-        return JsonResponseError('Không thuộc phòng của bạn')
-    userForChanged.is_active=not userForChanged.is_active
-    userForChanged.save()
-    
-    return JsonResponseSuccess('Thành công')
-def delete_user(request):
-    if request.user.groups.filter(name=LIST_GROUPS[1]).count() == 0:
-        return JsonResponseError('Không đủ thẩm quyền')
-    userId=request.POST.get('userId','')
-    if not userId:
-        return JsonResponseError('Không đủ dữ liệu')
-    userDelete=User.objects.get(id=userId)
-    if userDelete.groups.filter(name=LIST_GROUPS[1]).count() >0:
-        msg='Không thể xoá người dùng Trưởng Phòng'
-        return JsonResponseError(msg)
-    log_msg=request.user.first_name+" đã xoá người dùng "+userDelete.first_name +" ("+userDelete.username+")"
-    userDelete.delete()
-    #Ghi log
-    log_action(request.user,log_msg,action_type=db.ACTION_TYPES.DELETION)
-    
-    return JsonResponseSuccess('Thành công')
-@login_required
-def profile(request):
+def profile_view(request):
     userName=request.GET.get('u','')
     selfProfile=False
     if (request.user.username==userName):
         selfProfile=True
-    userInfo = db.User_info.objects.filter(user=User.objects.get(username=userName)).select_related('user')[0]
-    originalUser=User.objects.get(username=userName)
-    totalXfile=db.Xfile_role.objects.filter(user=originalUser,role=db.XFILE_ROLES.CREATOR).count()
-    group_name=list(userInfo.user.groups.all())
+    user = get_object_or_404(User.objects.all().select_related('info').prefetch_related('xfiles_can_edit'), username=userName)
+    userInfo = user.info
+
+    totalXfile = user.xfiles_can_edit.count()
+
+    group_name = userInfo.department.name
     title='Trang cá nhân'
     context={
         'user':{
-            'username':userInfo.user.username,
-            'first_name':userInfo.user.first_name,
-            'last_login':userInfo.user.last_login,
-            'is_active':userInfo.user.is_active,
-            'group_name':str(group_name[0].name),
-            'totalXfile':totalXfile
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_login': user.last_login,
+            'is_active': user.is_active,
+            'group_name': userInfo.position.name,
+            'totalXfile': totalXfile
         },
         'userInfo':{
             'skill':userInfo.skill,
             'address':userInfo.address,
             'phone_number':userInfo.phone_number,
-            'self_introdution':userInfo.self_introdution
+            'self_introduction':userInfo.self_introduction
         },
         'user_layout':request.user,
         'breadcrumb':title,
         'h1header':title,
-
     }
     if selfProfile:
         context['selfProfile']=1
-    context['user_role']=get_user_role(request.user)
+    context['user_role']=str(request.user.info.position.id)
     context['user_layout']=request.user
     # print(context)
-    return render(request, 'profile.html',context)
+    return render(request, 'users/profile.html',context)
 
-@login_required
-def edit_user_info(request):
-    first_name=request.POST.get('first_name','')
-    if first_name:
-        request.user.first_name=first_name
-        request.user.save()
-    phone_number=request.POST.get('phone_number','')
-    address=request.POST.get('address','')
-    self_introdution=request.POST.get('self_introdution','')
-    skill=request.POST.get('skill','')
-    userInfo=db.User_info.objects.get(user=request.user)
-    userInfo.phone_number=phone_number
-    userInfo.address=address
-    userInfo.self_introdution=self_introdution
-    userInfo.skill=skill
-    userInfo.save()
-    return redirect('/profile?u='+request.user.username)
-@login_required
-def change_password(request):
-    oldPassword=request.POST.get('oldPwd','')
-    newPassword=request.POST.get('newPwd','')
-    if not (newPassword and oldPassword):
-        return JsonResponseError('Thiếu dữ liệu')
-    if not check_password(oldPassword,request.user.password):  
-        return JsonResponseError('Mật khẩu cũ không chính xác')
-    if len(newPassword) <8:
-        return JsonResponseError('Mật khẩu mới phải lớn hơn 8 ký tự')
-    request.user.set_password(newPassword)
-    request.user.save()
-    return JsonResponseSuccess('Đổi mật khẩu thành công')
-@login_required
-def user_list_view(request):
-    title='Danh sách người dùng'
-    context={
-        'breadcrumb': title,
-        'h1header': title,
-        'userGroup': request.user.info.position.name,
-        'user_role': request.user.info.position.id,
-        'user_layout': request.user
-    }
-    return render(request, 'users/user-list.html', context)
+
 
 # Old views here
 def display_form_error(request, errors):
