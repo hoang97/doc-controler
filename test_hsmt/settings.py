632db97716fcb589ac0c8134b06b8abf1e1bdbc0
thienlongtpct <thienlongtpct@gmail.com>
2021-08-31 14:11:33 +0300

    Update

--- test_hsmt/settings.py
+++ test_hsmt/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # 3rd party apps
     'channels',
     'django_fsm',
+    "corsheaders",
     # my own apps
     'users',
     'hsmt',
@@ -50,8 +51,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
